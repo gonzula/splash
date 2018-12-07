81c80678e64d6fd252b8219ff1e49d7fcfd050f9
Gonzo <carloseaf@gmail.com>
2018-12-07 17:22:57 -0200

    Add xml_escape

--- utils.c
+++ utils.c
@@ -1,11 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
 #include "utils.h"
 
 #define CHARS "0123456789ABCDEF"
 
-void uuid_gen(char out[]) {
+void
+uuid_gen(char out[]) {
     static char seeded = 0;
     if (!seeded) {
         srand(time(NULL));
@@ -27,4 +29,42 @@ void uuid_gen(char out[]) {
 
         out[i] = c;
     }
+}
+
+char *
+xml_escape(char *str) {
+    /*    https://stackoverflow.com/a/1091953/1186116     */
+    /*     "   &quot;       */
+    /*     '   &apos;       */
+    /*     <   &lt;         */
+    /*     >   &gt;         */
+    /*     &   &amp;        */
+    char *escaped = (char *)calloc(strlen(str) * 6 + 1, sizeof(char));  // 6 is the longest escape sequence's length
+    int j = 0;
+    for (int i = 0; str[i]; i++) {
+        char to_replace[10];
+        to_replace[0] = 0;
+        if (str[i] == '\"') {
+            strcpy(to_replace, "&quot;");
+        } else if (str[i] == '\'') {
+            strcpy(to_replace, "&apos;");
+        } else if (str[i] == '<') {
+            strcpy(to_replace, "&lt;");
+        } else if (str[i] == '>') {
+            strcpy(to_replace, "&gt;");
+        } else if (str[i] == '&') {
+            strcpy(to_replace, "&amp;");
+        }
+
+        if (to_replace[0]) {
+            strcpy(escaped + j, to_replace);
+            j += strlen(to_replace);
+        } else {
+            escaped[j] = str[i];
+            j++;
+        }
+    }
+    escaped[j] = 0;
+
+    return escaped;
 }
--- utils.h
+++ utils.h
@@ -1,1 +1,2 @@
 void uuid_gen(char out[]);
+char *xml_escape(char *str);
