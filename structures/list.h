a349cf43eed226c8a578526068ad99fdb6d78475
Gonzo Fialho <carloseaf@gmail.com>
2019-02-23 16:49:30 -0300

    (=) coding stye

--- structures/list.h
+++ structures/list.h
@@ -3,15 +3,13 @@
 
 #define LIST_LOOP(_list) for (Node *node = _list->firstNode; node; node = node->next)
 
-typedef struct _node
-{
+typedef struct _node {
     void * object;
     struct _node *prev;
     struct _node *next;
 } Node;
 
-typedef struct
-{
+typedef struct {
     Node * firstNode;
     Node * lastNode;
     int count;
