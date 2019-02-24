9ba3c47a47ce24b78575e212bd74e00391ac770a
Gonzo Fialho <carloseaf@gmail.com>
2019-02-24 16:21:08 -0300

    (=) coding style

--- structures/str.h
+++ structures/str.h
@@ -11,14 +11,13 @@ typedef struct
     size_t bufferSize;
 } String;
 
-String * str_init();
-String * str_create(const char * original);
-String * str_escape_cstring(char * string);
-String * str_escape(String *str);
+String *str_init();
+String *str_create(const char * original);
+String *str_escape_cstring(char * string);
+String *str_escape(String *str);
 void str_append(String *str, const char * toAppend);
 void str_append_char(String *str, const unsigned char c);
 void str_center(String *str, int size);
 int str_unicode_len(String *str);
-
 
 #endif //__str_H_
