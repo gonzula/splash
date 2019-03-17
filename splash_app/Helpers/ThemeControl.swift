eea15f25b75e09c77a5eb7f9599be8787e9f0ec7
Gonzo Fialho <carloseaf@gmail.com>
2019-03-17 15:53:18 -0300

    (+) update theme control on theme change

--- splash_app/Helpers/ThemeControl.swift
+++ splash_app/Helpers/ThemeControl.swift
@@ -9,23 +9,44 @@
 import UIKit
 
 class ThemeControl: UISegmentedControl {
+
+    var observers = [Any]()
+
     init() {
         let themes = ThemeManager.Theme.allCases
         super.init(items: themes.map {$0.humanName})
-        let selectedTheme = UserDefaults.standard.lastUsedTheme
-        let index = themes.firstIndex(of: selectedTheme)
-        selectedSegmentIndex = index ?? -1
+        setValue()
 
         addTarget(self,
                   action: #selector(changeTheme),
                   for: .valueChanged)
+
+        observers.append( // swiftlint:disable:next discarded_notification_center_observer
+            NotificationCenter.default.addObserver(forName: .themeChanged,
+                                                   object: nil,
+                                                   queue: nil,
+                                                   using: { [weak self] _ in
+                                                    self?.setValue()
+            }))
     }
 
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
 
+    deinit {
+        observers.forEach(NotificationCenter.default.removeObserver)
+    }
+
     required init?(coder aDecoder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+
+    func setValue() {
+        let themes = ThemeManager.Theme.allCases
+        let selectedTheme = UserDefaults.standard.lastUsedTheme
+        let index = themes.firstIndex(of: selectedTheme)
+        guard self.selectedSegmentIndex != index else {return}
+        selectedSegmentIndex = index ?? -1
+    }
 
     // MARK: - User Interaction
 
